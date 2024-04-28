dd1d5757cecca4fac857710f25fec8df149e3f11
hundredgolds <hundred.golds@gmail.com>
2024-04-28 11:52:05 +0500

    svchnguz

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    sidebar.classList.toggle("active");
+}
+function openCategory(url) {
+    window.location.href = url;
+  }
